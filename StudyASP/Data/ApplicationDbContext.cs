1f5a6ad7787a9c7ca33d71242283b81c98a31399
Thien260102 <nguyenvanthien18209@gmail.com>
2023-08-12 13:54:15 +0700

    Configuring some packages and Database

---
+++ StudyASP/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudyASP.Model;
+
+namespace StudyASP.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
