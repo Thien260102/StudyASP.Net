a2ca038b024fbf5fec38bf59720779946f3646c8
Thien260102 <nguyenvanthien18209@gmail.com>
2023-08-13 19:35:55 +0700

    Simple display name and range annotation

--- StudyASP/Model/Category.cs
+++ StudyASP/Model/Category.cs
@@ -6,9 +6,12 @@ namespace StudyASP.Model
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
 
+        [Display(Name = "Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be in range 1 - 100!")]
         public int DisplayOrder { get; set; }
     }
 }
